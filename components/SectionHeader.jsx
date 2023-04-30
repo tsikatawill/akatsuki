6c41e93dae26e9ff96a71da67b50e5e2407728fe
William M. Tsikata <tsikatawill@gmail.com>
2023-04-30 14:38:53 +0000

    add section header cmp

---
+++ components/SectionHeader.jsx
@@ -0,0 +1,20 @@
+import { styled } from "@/stitches.config";
+import { Heading, Text } from ".";
+
+export const SectionHeader = ({ title, description }) => {
+  return (
+    <Header>
+      <Heading>{title}</Heading>
+      <Text>{description}</Text>
+    </Header>
+  );
+};
+
+const Header = styled("div", {
+  textAlign: "center",
+  marginBottom: 64,
+  maxWidth: 1167,
+  display: "flex",
+  flexDirection: "column",
+  gap: 24,
+});
