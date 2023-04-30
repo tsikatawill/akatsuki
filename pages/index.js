3dfb4dbe4ab1c5bfc05584176403ef152861a728
William M. Tsikata <tsikatawill@gmail.com>
2023-04-30 10:59:20 +0000

    add about section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Container, Hero, Navbar } from "@/components";
+import { About, Container, Hero, Navbar, Text } from "@/components";
 
 export default function Home() {
   return (
@@ -16,67 +16,39 @@ export default function Home() {
       <main>
         <Container>
           <Hero />
+          <About />
           <br />
           <br />
           <br />
           <br />
           <br />
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
-          beatae explicabo praesentium animi deserunt dolore similique rerum
-          eveniet incidunt. Labore corrupti culpa officia, modi iure magni,
-          voluptate exercitationem inventore dolore ex voluptas nisi officiis
-          nihil velit suscipit nulla mollitia. Debitis, corrupti. Commodi ad
-          neque nulla velit quas adipisci quo vel aspernatur totam, placeat
-          explicabo? Quos fugiat corrupti quis iure rem maiores eligendi
-          voluptatibus dicta, reprehenderit quibusdam accusamus veniam impedit
-          commodi soluta blanditiis doloremque. Temporibus consectetur, modi
-          commodi nobis fuga ipsa ullam a pariatur earum ut tenetur deserunt et
-          perspiciatis obcaecati itaque? Aperiam sit, vitae dolorem impedit
-          quibusdam nesciunt provident veniam sunt. Deserunt ipsa ipsam odio
-          quibusdam quo rerum, esse assumenda amet? Animi, nostrum consequatur
-          sunt facere impedit excepturi quos accusantium odio nisi magni velit
-          alias aliquid eius totam rem rerum, suscipit repudiandae, beatae
-          deleniti laudantium perferendis vitae non! Quod modi corporis est
-          laborum voluptatibus quasi facilis assumenda consequuntur. Soluta
-          nulla esse, perferendis laborum modi harum autem illo, quibusdam
-          labore animi, nisi officiis nemo tempore praesentium? Eligendi, velit
-          voluptatibus earum praesentium aspernatur ipsum tempora sapiente totam
-          vel. Mollitia, fugit. Nemo impedit quidem fugiat dolorem quo atque
-          nisi nostrum, dignissimos iure perferendis est accusamus vel, alias id
-          eius expedita voluptatem voluptatum ratione cumque debitis deleniti.
-          Animi dicta suscipit illo ducimus necessitatibus! Explicabo, rerum
-          voluptatum. Corrupti a accusamus et quam. Provident tempora excepturi
-          itaque, incidunt officiis, maxime asperiores, optio aperiam amet hic
-          natus beatae. Mollitia doloribus ducimus asperiores a aut odio saepe
-          itaque amet iure libero dignissimos facilis perspiciatis nihil unde
-          facere sit consectetur id magni natus, cum fugiat. Quos saepe fugit
-          aspernatur ipsam, minus delectus quae eligendi illum numquam odio
-          beatae earum sit alias eius accusamus laboriosam nemo sunt quis
-          laudantium omnis eos, perspiciatis nihil aut. Fuga animi quasi earum
-          nihil odit exercitationem similique cupiditate doloribus saepe laborum
-          neque soluta, consequuntur atque mollitia quaerat harum fugit
-          voluptatibus non rem ipsa et. Fuga voluptate repellat obcaecati
-          facilis asperiores deleniti cumque officiis modi tempore! Totam dolore
-          molestias ea dicta qui! Dolor quasi id minus, dicta, provident odio
-          dolorem quaerat excepturi ratione corrupti mollitia vero doloremque
-          dolore consectetur explicabo, maxime consequatur quisquam ipsa ex
-          labore? Eligendi debitis iusto nostrum aliquam esse quo dolores
-          reiciendis natus, cumque iure velit magni doloribus pariatur dolore
-          magnam quam, doloremque, voluptate rem consectetur modi? Fugiat esse
-          adipisci sed vel! Eaque recusandae quas minima aspernatur quis nam
-          similique laboriosam repellendus temporibus consectetur vel, dolores
-          sapiente? Aut velit, qui magnam beatae debitis deserunt iusto maxime
-          rem ipsa inventore in non. Eos, corrupti. Tempore exercitationem ut
-          iure recusandae possimus eos distinctio voluptatum incidunt ipsa sed,
-          officia laudantium sapiente quae nihil velit consequuntur fuga,
-          eveniet doloremque? Corporis, minus. Voluptatum ad repellat laborum
-          asperiores delectus explicabo ab et eum repellendus recusandae
-          provident amet magnam natus saepe dicta, alias quae, id unde quam
-          cupiditate sequi deserunt dignissimos. Fuga iure quisquam, recusandae
-          beatae et velit rerum alias magnam culpa, impedit libero ea obcaecati?
-          Vero soluta sint animi dignissimos excepturi harum sapiente
-          voluptates, sit autem rerum explicabo atque culpa blanditiis debitis
-          ex, aut expedita, dolor esse voluptate nostrum! ipsa odit?
+          <Text>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
+            beatae explicabo praesentium animi deserunt dolore similique rerum
+            eveniet incidunt. Labore corrupti culpa officia, modi iure magni,
+            voluptate exercitationem inventore dolore ex voluptas nisi officiis
+            nihil velit suscipit nulla mollitia. Debitis, corrupti. Commodi ad
+            neque nulla velit quas adipisci quo vel aspernatur totam, placeat
+            explicabo? Quos fugiat corrupti quis iure rem maiores eligendi
+            voluptatibus dicta, reprehenderit quibusdam accusamus veniam impedit
+            commodi soluta blanditiis doloremque. Temporibus consectetur, modi
+            commodi nobis fuga ipsa ullam a pariatur earum ut tenetur deserunt
+            et perspiciatis obcaecati itaque? Aperiam sit, vitae dolorem impedit
+            quibusdam nesciunt provident veniam sunt. Deserunt ipsa ipsam odio
+            quibusdam quo rerum, esse assumenda amet? Animi, nostrum consequatur
+            sunt facere impedit excepturi quos accusantium odio nisi magni velit
+            alias aliquid eius totam rem rerum, suscipit repudiandae, beatae
+            deleniti laudantium perferendis vitae non! Quod modi corporis est
+            laborum voluptatibus quasi facilis assumenda consequuntur. Soluta
+            nulla esse, perferendis laborum modi harum autem illo, quibusdam
+            labore animi, nisi officiis nemo tempore praesentium? Eligendi,
+            velit voluptatibus earum praesentium aspernatur ipsum tempora
+            sapiente totam vel. Mollitia, fugit. Nemo impedit quidem fugiat
+            dolorem quo atque nisi nostrum, dignissimos iure perferendis est
+            accusamus vel, alias id eius expedita voluptatem voluptatum ratione
+            cumque debitis deleniti. Animi dicta suscipit illo ducimus
+            necessitatibus! Explicabo, rerum
+          </Text>
         </Container>
       </main>
     </>
