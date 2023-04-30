f4ec5cb9f07e08d89482db253bcf411db1549255
William M. Tsikata <tsikatawill@gmail.com>
2023-04-30 14:43:57 +0000

    export headbands

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -1,3 +1,30 @@
+import { styled } from "@/stitches.config";
+import Image from "next/image";
+import { HeadbandRow, SectionHeader } from ".";
+
 export const Welcome = () => {
-  return <div>Welcome</div>;
+  return (
+    <Wrapper>
+      <SectionHeader
+        title="All rogue ninjas are welcome"
+        description="We don&rsquo;t discriminate, everyone is welcomed to join irrespective of your clan, village, race or federation."
+      />
+
+      <Headbands>
+        <HeadbandRow />
+        <HeadbandRow delay scrollFrom="left" />
+      </Headbands>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled("section", {
+  marginTop: 100,
+});
+
+const Headbands = styled("div", {
+  marginTop: 12,
+  display: "flex",
+  flexDirection: "column",
+  gap: 40,
+});
